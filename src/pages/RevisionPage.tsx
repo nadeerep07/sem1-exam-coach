d4df857d778afa96913effd5501957c10cd89cac
NADEER E P <nadeerep07@gmail.com>
2026-08-20 17:13:38 +0530

    docs: Add module comments & documentation to RevisionPage.tsx

--- src/pages/RevisionPage.tsx
+++ src/pages/RevisionPage.tsx
@@ -1,3 +1,8 @@
+/**
+ * @file RevisionPage.tsx
+ * @description Module implementation for BCA Sem 1 Exam Coach platform.
+ * @module src/pages/RevisionPage.tsx
+ */
 import React, { useState } from 'react';
 import { QuestionItem, SubjectMeta } from '../types/exam';
 import { QuickRevisionView } from '../components/subject/QuickRevisionView';
